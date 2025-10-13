d0774512df8e1ca27bcaf9456a54e8a65b2d9d72
Y <chrisyockey88@gmail.com>
2025-10-13 12:34:23 -0700

    added steamclient

---
+++ other-network/src/steam-client/steam_client.cpp
@@ -0,0 +1,10 @@
+/**
+ * \file steam-client/steam_client.cpp
+ **/
+#include "steam-client/steam_client.hpp"
+
+#include <steam/steam_api.h>
+
+namespace other {
+
+}  // namespace other
---
+++ other-network/src/steam-client/steam_client.hpp
@@ -0,0 +1,20 @@
+/**
+ * \file  steam-client/steam_client.hpp
+ **/
+#ifndef OTHER_NETWORK_STEAM_CLIENT_STEAM_CLIENT_HPP
+#define OTHER_NETWORK_STEAM_CLIENT_STEAM_CLIENT_HPP
+
+#include <cstdint>
+
+namespace other {
+
+  // class SteamApi
+
+  class steam_client {
+   public:
+    uint32_t app_id;
+  };
+
+}  // namespace other
+
+#endif  // OTHER_NETWORK_STEAM_CLIENT_STEAM_CLIENT_HPP
