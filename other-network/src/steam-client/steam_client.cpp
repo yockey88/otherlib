3d525bc63efe855d58eb5bb2083ad29e285868c1
Y <chrisyockey88@gmail.com>
2025-10-14 17:57:17 -0700

    added steamclient class

--- other-network/src/steam-client/steam_client.cpp
+++ other-network/src/steam-client/steam_client.cpp
@@ -7,4 +7,25 @@
 
 namespace other {
 
+  void steam_client::initialize_steam_api() {
+    bool require_steam = environment_cfg.get_project_value("steam-api", "require-steam");
+
+    if (!(SteamAPI_Init())) {
+      CORE_LOG_ERROR("Failed to initialize Steam API");
+      state.initialized = false;
+      return;
+    } else {
+      CORE_LOG_INFO("Successfully initialized Steam API");
+      state.initialized = true;
+    }
+
+    // state.requires_restart = SteamAPI_RestartAppIfNecessary(app_id);
+  }
+
+  void steam_client::shutdown_steam_api() {
+    if (state.initialized) {
+      SteamAPI_Shutdown();
+    }
+  }
+
 }  // namespace other
--- other-network/src/steam-client/steam_client.hpp
+++ other-network/src/steam-client/steam_client.hpp
@@ -6,13 +6,29 @@
 
 #include <cstdint>
 
+#include "core/config_table.hpp"
+
 namespace other {
 
   // class SteamApi
 
   class steam_client {
    public:
-    uint32_t app_id;
+    steam_client(const config_table& config)
+        : environment_cfg(config) {}
+    ~steam_client() {}
+
+    void initialize_steam_api();
+    void shutdown_steam_api();
+
+   private:
+    const config_table& environment_cfg;
+
+    struct api_state {
+      bool initialized = false;
+      bool requires_restart = false;
+    };
+    api_state state;
   };
 
 }  // namespace other
