6fdb5c650dc02654304d8a297d86bbbf5ad45033
Y <chrisyockey88@gmail.com>
2026-01-18 07:21:14 -0800

    comment

--- otherlib/src/ui/property_inspector_node.cpp
+++ otherlib/src/ui/property_inspector_node.cpp
@@ -64,6 +64,7 @@ namespace other {
       }
 
       if (ImGui::CollapsingHeader(component_name.data())) {
+        /// this does not look good, but works for now
         auto settings = type_settings<T>::get();
         ImReflect::Input(component_name.data(), *active_scene->get_component<T>(object), settings);
       }
