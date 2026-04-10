795c897b6e496a50b64f5335615d7dd5d293b6e8
Y <chrisyockey88@gmail.com>
2026-04-09 17:37:47 -0700

    :.(

--- other-physics/src/physics/backends/jolt_api.cpp
+++ other-physics/src/physics/backends/jolt_api.cpp
@@ -8,6 +8,9 @@
 #include "physics_world/physics_world.hpp"
 
 // clang-format off
+#define JPH_FLOATING_POINT_EXCEPTIONS_ENABLED
+#define JPH_PROFILE_ENABLED
+#define JPH_OBJECT_STREAM
 #define JPH_SHARED_LIBRARY 
 // #define JPH_DOUBLE_PRECISION 
 #define JPH_DEBUG_RENDERER
