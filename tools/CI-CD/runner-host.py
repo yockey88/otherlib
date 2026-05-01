8b5babaada20d6b856c5d63c2c5436c8b71de89b
Y <chrisyockey88@gmail.com>
2026-05-01 11:05:41 -0700

    testing push listener

--- tools/CI-CD/runner-host.py
+++ tools/CI-CD/runner-host.py
@@ -29,6 +29,24 @@ async def handle_run_test_request(request):
     print(f"Unexpected error: {e}")
     return web.Response(status=500, text=f"Unexpected error: {e}")
 
+@routes.post('/run-test')
+async def handle_run_test_post_request(request):
+  try:
+    if not validate_run_request(request):
+      return web.Response(status=400, text="Invalid request")
+    else:
+      for key, value in request.headers.items():
+        print(f"Header: {key} = {value}")
+      return web.Response(status=200, text="Request validated successfully")
+    
+  except subprocess.CalledProcessError as e: 
+    print(f"Error executing CI/CD pipeline: {e}")
+    return web.Response(status=500, text=f"Error executing CI/CD pipeline: {e}")
+  
+  except Exception as e:
+    print(f"Unexpected error: {e}")
+    return web.Response(status=500, text=f"Unexpected error: {e}")
+
 # async def main():
 app = web.Application()
 app.add_routes(routes)
