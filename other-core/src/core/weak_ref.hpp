57f21e480c523729a08acb91d8ec0924d9fab3bb
Y <chrisyockey88@gmail.com>
2026-05-09 11:13:22 -0700

    made ref thread safe

--- other-core/src/core/weak_ref.hpp
+++ other-core/src/core/weak_ref.hpp
@@ -5,6 +5,14 @@
 #define OTHER_CORE_WEAK_REF_HPP
 
 namespace other {
+  namespace detail {
+
+    void register_reference(void* instance);
+    void remove_reference(void* instance);
+    bool is_valid_ref(void* instance);
+    size_t num_living_references();
+
+  }  // namespace detail
 
 }  // namespace other
 
