dd66c6aa18bbde1b23b98308d63bc6e03e38670f
Y <chrisyockey88@gmail.com>
2026-05-24 07:47:49 -0700

    primary render refator complete

--- otherlib/src/driver/systems/rendering_system.cpp
+++ otherlib/src/driver/systems/rendering_system.cpp
@@ -5,8 +5,6 @@
 
 #include <SDL3/SDL_dialog.h>
 
-#include "renderer/default_pass_executor_resolver.hpp"
-
 #include "driver/driver.hpp"
 #include "driver/environment_registry.hpp"
 #include "driver/systems/asset_system.hpp"
