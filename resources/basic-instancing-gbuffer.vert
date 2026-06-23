0caee585437cfb11ada2a9844d02ebd07e9728e1
Y <chrisyockey88@gmail.com>
2026-06-22 19:17:18 -0700

    changed shader names

--- resources/basic-instancing-gbuffer.vert
+++ resources/basic-instancing-gbuffer.vert
@@ -1,4 +1,4 @@
-#include "shader-modules/basic-instancing-vertex.glsl"
+#include "shader-modules/basic-geometry.glsl"
 
 out vec3 world_position;
 out vec3 world_normal;
--- resources/shader-modules/basic-material.glsl
+++ resources/shader-modules/basic-material.glsl
@@ -12,27 +12,10 @@ struct material {
   float shininess;
 };
 
-/// why does removing binding = 2 here break this?
-layout (std140, binding = 2) uniform camera_buffer {
-  vec4 camera_position;
-  vec4 camera_forward;
-
-  /// near & far clip, defocus_angle padding x2
-  vec4 camera_features;
-
-  vec4 defocus_disk_u;
-  vec4 defocus_disk_v;
-
-  mat4 view_matrix;
-  mat4 projection_matrix;
-};
-
 layout (std430) readonly buffer material_buffer {
   material materials[];
 };
 
-uniform int OE_num_point_lights;
-uniform int OE_num_direction_lights;
 flat in int OE_material_index;
 
 material get_instance_material() {
