964241f01a980f717ec185328747ff3395afa547
Y <chrisyockey88@gmail.com>
2026-08-04 20:43:23 -0700

    physics update

--- test-project/assets/scripts/TestBehavior.cs
+++ test-project/assets/scripts/TestBehavior.cs
@@ -6,26 +6,6 @@ public class TestBehavior : SceneBehavior
   float rotation = 0.0f;
   float rotation_speed = 90.0f; // degrees per second
 
-  protected override void OnAwake()
-  {
-    Debug.Warn("TestBehavior: OnAwake called.");
-  }
-
-  protected override void OnRemove()
-  {
-    Debug.Warn("TestBehavior: OnRemove called.");
-  }
-
-  protected override void OnEnable()
-  {
-    Debug.Warn("TestBehavior: OnEnable called.");
-  }
-
-  protected override void OnDisable()
-  {
-    Debug.Warn("TestBehavior: OnDisable called.");
-  }
-
   protected override void OnUpdate()
   {
     Transform.EulerAngles += Vec3.Up * rotation_speed * Time.DeltaTime;
